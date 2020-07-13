fbc30dd64041c56c7eb8ce524d977ffb6e57aff2
Ravleen Kaur <ravleenkaurtumber@gmail.com>
2020-07-12 21:51:58 -0400

    added teresstial planets class!

---
+++ AssignmentPlanets/TerrestrialPlanet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentPlanets
+{
+    class TerrestrialPlanet:Planet
+    {
+        //PRIVATE INSTANCE VARIABLES
+        private bool m_oxygen;
+        //PUBLIC PROPERTIES
+        //CONSTRUCTOR(S)
+        public TerrestrialPlanet(string name, double diameter, double mass,bool oxygen)
+            :base(name,diameter,mass)
+        {
+            m_oxygen = oxygen; 
+        }
+        //PUBLIC METHODS
+        public bool HasMoons()
+        {
+            if (MoonCount > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Habitable()
+        {
+            if(m_oxygen==true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //PRIVATE METHODS
+    }
+}
