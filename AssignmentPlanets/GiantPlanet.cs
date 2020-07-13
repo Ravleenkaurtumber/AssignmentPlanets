c85d71f307c2b9a1d2a9331b31e0c529ed038e9a
Ravleen Kaur <ravleenkaurtumber@gmail.com>
2020-07-12 20:22:12 -0400

    created a class gaintplanet inherited from planets

---
+++ AssignmentPlanets/GiantPlanet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentPlanets
+{
+    class GiantPlanet:Planet
+    {
+
+        //PRIVATE INSTANCE VARIABLES(FIELDS)
+        private string m_type;
+        //PUBLIC PROPERTIES
+        
+         //CONSTRUCTOR(S)
+         public GiantPlanet(string name, double diameter, double mass, string type)
+            :base(name,diameter,mass)
+        {
+            m_type = type;
+        }
+        //PUBLIC METHODS
+        public  bool HasMoons()
+        {
+        if(MoonCount>0)
+            {
+                return true;
+            }
+            else
+            {
+            return false;
+            }
+        }
+        public bool HasRings()
+        {
+            if(RingCount>0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //PRIVATE METHODS
+
+    }
+}
